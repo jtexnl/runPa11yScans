e9943eb2e952c5a3c79656bd67a72ad20133018a
jtexnl <jtexnl@gmail.com>
2016-05-12 15:35:55 -0400

    Add .csv functionality

--- scripts/runPa11yScan.py
+++ scripts/runPa11yScan.py
@@ -1,10 +1,30 @@
 import os
 import threading
 import json
+import collections
 
 def scan(domain):
-    resp = os.system('pa11y --ignore "warning;notice" --reporter json ' + domain + ' > ' + domain + '.json')
+    resp = os.system('pa11y --ignore "warning;notice" --reporter csv ' + domain + ' > ' + domain + '.csv')
     return resp
+
+def loadAndClean(doc):
+    with open(doc) as infile:
+        data = list(infile)
+    output = []
+    for row in data:
+        output.append(row.replace('"', '').replace('\n', '').split(','))
+    return output
+
+def jsonify(dataSet):
+    titleRow = dataSet[0]
+    ln = len(titleRow) - 1
+    outputList = []
+    for row in dataSet[1:len(dataSet)]:
+        output = collections.OrderedDict()
+        for i in range(0, ln):
+            output[titleRow[i]] = row[i]
+        outputList.append(output)
+    return outputList
 
 def runScans(domainList, numThreads):
     threads = numThreads
@@ -12,9 +32,10 @@ def runScans(domainList, numThreads):
     jobs = []
     for domain in domainList:
         thread = threading.Thread(target = scan(domain))
-        data = json.load(open(domain + '.json'))
+        temp = loadAndClean(domain + '.csv')
+        data = jsonify(temp)
         masterData[domain] = data
-        os.system('rm ' + domain + '.json')
+        os.system('rm ' + domain + '.csv')
     return masterData
 
 def writeJson(inputData, fileName):
