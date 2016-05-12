158da33b097a68352c1d9f6e0bebe8b8c45b04b8
jtexnl <jtexnl@gmail.com>
2016-05-12 11:00:56 -0400

    adding multithreading

--- scripts/runPa11yScan.py
+++ scripts/runPa11yScan.py
@@ -1,20 +1,28 @@
 import os
 import threading
+import json
 
 def scan(domain):
     resp = os.system('pa11y --ignore "warning;notice" --reporter json ' + domain + ' > ' + domain + '.json')
     return resp
 
-def runScans(domainList):
+def runScans(domainList, numThreads):
+    threads = numThreads
     masterData = {}
+    jobs = []
     for domain in domainList:
-        scan(domain)
+        thread = threading.Thread(target = scan(domain))
         data = json.load(open(domain + '.json'))
         masterData[domain] = data
         os.system('rm ' + domain + '.json')
     return masterData
 
-domainList = ['gsa.gov', 'state.gov', 'cio.gov', 'dhs.gov']
+def writeJson(inputData, fileName):
+    with open(fileName, 'w+') as outfile:
+        json.dump(inputData, outfile, indent = 4)
+
+domainList = ['gsa.gov', 'state.gov', 'cio.gov', 'dhs.gov', 'defense.gov', 'abmc.gov', 'aging.gov']
 
 if __name__ == '__main__':
-    runScans(domainList)
+    data = runScans(domainList, 10)
+    writeJson(data, 'test.json')
