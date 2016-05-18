ed0be7d5df1e8774c1173fbbd974b0edbb10e72c
jtexnl <jtexnl@gmail.com>
2016-05-18 10:46:19 -0400

    adding documentation to the script

--- scripts/runPa11yScan.py
+++ scripts/runPa11yScan.py
@@ -6,10 +6,13 @@ import csv
 import datetime
 
 class runner():
+
     def __init__(self):
         self.date = datetime.datetime.now().strftime('%Y%m%d')
         self.domains = self.loadDomains('../data/domains.csv')
         self.output = self.runScans(self.domains, 10)
+
+    #Loads the domain file and cleans up the output, yielding a clean list
     def loadDomains(self, path):
         with open(path) as infile:
             data = list(infile)
@@ -17,9 +20,13 @@ class runner():
         for row in data[1:len(data)]:
             clean.append(row.replace('\n', '').split(','))
         return clean
+
+    #Runs the bash command to scan a single domain, yielding a csv report. json is also available, but is not working on AWS.
     def scan(self, domain):
         resp = os.system('pa11y --ignore "warning;notice" --reporter csv ' + domain + ' > ' + domain + '.csv')
         return resp
+
+    #Function for loading one of the scanner output reports and cleaning it up
     def loadAndClean(self, doc):
         with open(doc) as infile:
             data = list(infile)
@@ -27,6 +34,8 @@ class runner():
         for row in data:
             output.append(row.replace('"', '').replace('\n', '').split(','))
         return output
+
+    #Converts a .csv file into an ordered dict, which will be written to json using the writeJson function
     def jsonify(self, dataSet):
         titleRow = dataSet[0]
         ln = len(titleRow) - 1
@@ -37,6 +46,8 @@ class runner():
                 output[titleRow[i]] = row[i]
             outputList.append(output)
         return outputList
+
+    #Function to run the scanner on a list of domains. Threading functions need some work. Outputs the data as a dict.
     def runScans(self, domainList, numThreads):
         threads = numThreads
         masterData = {}
@@ -45,10 +56,15 @@ class runner():
             site = row[0].lower()
             thread = threading.Thread(target = self.scan(site))
             temp = self.loadAndClean(site + '.csv')
-            data = self.jsonify(temp)
+            try:
+                data = self.jsonify(temp)
+            except IndexError:
+                data = 'File Error'
             masterData[site] = data
             os.system('rm ' + site + '.csv')
         return masterData
+
+    #Prettifies and writes an ordered dict to a json file
     def writeJson(self, inputData, fileName):
         with open(fileName, 'w+') as outfile:
             json.dump(inputData, outfile, indent = 4)
